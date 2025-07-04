2d313717bf365c65a09d972b30946581c5d3f527
Gutu Rarie <guturarie@gmail.com>
2025-07-04 15:13:14 +0300

    last all changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+  AOS.init({ duration: 1000, once: true });
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-white dark:bg-black text-gray-900 dark:text-white">
+      <Navbar />
+      <main className="pt-20">
+        <Home />
+        <About />
+        < Projects />
+        <Contact />
+      </main>
     </div>
   );
 }
